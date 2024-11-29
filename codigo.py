5785c5c78d255d552fdffeb030c489e6ce35873e
DaviRodriFv <davir2493@gmail.com>
2024-11-29 19:31:34 -0300

    Commit Historico

--- codigo.py
+++ codigo.py
@@ -72,4 +72,24 @@ while True:
                print(item)
        else:
            print("Nenhum cálculo realizado ainda.")
+            elif menu == 0:
+        print("Saindo do software...")
+        break
+
+    else:
+        print("Opção invalida, encerrando programa...")
+        break
+
+    print()
+    print("Deseja continuar utilizando o software?")
+    print("1 - Sim")
+    print("2 - Não")
+    continuar = int(input("Escolha a opção desejada: "))
+    
+    if continuar == 1:
+        limpar_tela()
+    else:
+        print("Encerrando o programa...")
+        break
+
 
